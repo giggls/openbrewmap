b307261783986de544b9a9ae15fe93bfc6e462aa
Sven Geggus <sven-osm@geggus.net>
2021-08-17 13:56:08 +0200

    Add support for multiple values in craft and industrial.
    Examples:
    
    craft=brewery;distillery
    craft=winery;brewery;distillery

--- gen_view_poi_breweries.sql
+++ gen_view_poi_breweries.sql
@@ -7,14 +7,14 @@ CREATE VIEW osm_poi_breweries AS
          geom,
          unify_tags(tags,geom) as tags,
          'node' as osm_type,
-         CASE WHEN (tags->'industrial' = 'brewery') then 'industrial'
+         CASE WHEN (tags ? 'industrial') then 'industrial'
               WHEN ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') then 'micro'
          ELSE 'craft' END AS category
   FROM  osm_poi_point
   WHERE
     ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') OR
-    (tags->'industrial' = 'brewery') OR
-    (tags->'craft' = 'brewery')
+    ((tags ? 'industrial') AND (tags->'industrial' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')) OR
+    ((tags ? 'craft') AND (tags->'craft' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$'))
   UNION ALL
   -- multipolygon
   SELECT (-1*osm_id) as osm_id,
@@ -22,14 +22,14 @@ CREATE VIEW osm_poi_breweries AS
          geom,
          unify_tags(tags,geom) as tags,
          'way' as osm_type,
-         CASE WHEN (tags->'industrial' = 'brewery') then 'industrial'
+         CASE WHEN (tags ? 'industrial') then 'industrial'
               WHEN ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') then 'micro'
          ELSE 'craft' END AS category
   FROM  osm_poi_poly
   WHERE (
     ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') OR
-    (tags->'industrial' = 'brewery') OR
-    (tags->'craft' = 'brewery')
+    ((tags ? 'industrial') AND (tags->'industrial' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')) OR
+    ((tags ? 'craft') AND (tags->'craft' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$'))
   ) AND (osm_id < 0) AND (osm_id > -1e17)
   UNION ALL
   -- closed way
@@ -38,14 +38,14 @@ CREATE VIEW osm_poi_breweries AS
          geom,
          unify_tags(tags,geom) as tags,
          'relation' as osm_type,
-         CASE WHEN (tags->'industrial' = 'brewery') then 'industrial'
+         CASE WHEN (tags ? 'industrial') then 'industrial'
               WHEN ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') then 'micro'
          ELSE 'craft' END AS category
   FROM  osm_poi_poly
   WHERE (
     ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') OR
-    (tags->'industrial' = 'brewery') OR
-    (tags->'craft' = 'brewery')
+    ((tags ? 'industrial') AND (tags->'industrial' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')) OR
+    ((tags ? 'craft') AND (tags->'craft' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$'))
   ) AND (osm_id < -1e17);
 
 GRANT SELECT ON osm_poi_breweries to public;
