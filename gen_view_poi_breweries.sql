ee6a0ea01c4b6b1fd7b745969a2cea45c46e2bec
Sven Geggus <sven-osm@geggus.net>
2024-10-23 09:07:41 +0200

    htore -> jsonb

--- gen_view_poi_breweries.sql
+++ gen_view_poi_breweries.sql
@@ -7,13 +7,13 @@ CREATE VIEW osm_poi_breweries AS
          geom,
          unify_tags(tags,geom) as tags,
          CASE WHEN (tags ? 'industrial') then 'industrial'
-              WHEN ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') then 'micro'
+              WHEN ((tags->>'amenity' = 'restaurant') OR (tags->>'amenity' = 'pub') OR (tags->>'tourism' = 'hotel')) AND (tags->>'microbrewery' = 'yes') then 'micro'
          ELSE 'craft' END AS category
   FROM  osm_poi_point
   WHERE
-    ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') OR
-    ((tags ? 'industrial') AND (tags->'industrial' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')) OR
-    ((tags ? 'craft') AND (tags->'craft' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$'))
+    ((tags->>'amenity' = 'restaurant') OR (tags->>'amenity' = 'pub') OR (tags->>'tourism' = 'hotel')) AND (tags->>'microbrewery' = 'yes') OR
+    ((tags ? 'industrial') AND (tags->>'industrial' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')) OR
+    ((tags ? 'craft') AND (tags->>'craft' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$'))
   UNION ALL
   -- multipolygon or way
   SELECT osm_id,
@@ -21,12 +21,12 @@ CREATE VIEW osm_poi_breweries AS
          geom,
          unify_tags(tags,geom) as tags,
          CASE WHEN (tags ? 'industrial') then 'industrial'
-              WHEN ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') then 'micro'
+              WHEN ((tags->>'amenity' = 'restaurant') OR (tags->>'amenity' = 'pub') OR (tags->>'tourism' = 'hotel')) AND (tags->>'microbrewery' = 'yes') then 'micro'
          ELSE 'craft' END AS category
   FROM  osm_poi_poly
   WHERE (
-    ((tags->'amenity' = 'restaurant') OR (tags->'amenity' = 'pub') OR (tags->'tourism' = 'hotel')) AND (tags->'microbrewery' = 'yes') OR
-    ((tags ? 'industrial') AND (tags->'industrial' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')) OR
-    ((tags ? 'craft') AND (tags->'craft' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')));
+    ((tags->>'amenity' = 'restaurant') OR (tags->>'amenity' = 'pub') OR (tags->>'tourism' = 'hotel')) AND (tags->>'microbrewery' = 'yes') OR
+    ((tags ? 'industrial') AND (tags->>'industrial' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')) OR
+    ((tags ? 'craft') AND (tags->>'craft' ~ '^brewery *[,;]|[,;] *brewery *[,;]|[,;] *brewery$|^brewery$')));
 
 GRANT SELECT ON osm_poi_breweries to public;
